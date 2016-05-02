511d12aa28fdacf0a27aceda2fa8d95352e0b7bc
Marco Minerva <marco.minerva@gmail.com>
2016-05-02 12:58:36 +0200

    Libraries update

---
+++ Src/See4Me.Shared/ServiceKeys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace See4Me
+{
+    public static partial class ServiceKeys
+    {
+        public const string VisionSubscriptionKey = "1c95c8f0c7ef47f5b9e434cef329825e";
+        public const string TranslatorClientId = "see4me";
+        public const string TranslatorClientSecret = "XLDaWC8XrsJoZs8caG4Agr0K9Sk4at0z65H9NTkTHDM=";
+    }
+}
