2cf16c1e670d27299b9faeaf170afb29308d4c29
Marco Minerva <marco.minerva@gmail.com>
2016-05-02 13:01:59 +0200

    Update ServiceKeys.cs

--- Src/See4Me.Shared/ServiceKeys.cs
+++ Src/See4Me.Shared/ServiceKeys.cs
@@ -6,8 +6,8 @@ namespace See4Me
 {
     public static partial class ServiceKeys
     {
-        public const string VisionSubscriptionKey = "1c95c8f0c7ef47f5b9e434cef329825e";
-        public const string TranslatorClientId = "see4me";
-        public const string TranslatorClientSecret = "XLDaWC8XrsJoZs8caG4Agr0K9Sk4at0z65H9NTkTHDM=";
+        public const string VisionSubscriptionKey = "";
+        public const string TranslatorClientId = "";
+        public const string TranslatorClientSecret = "";
     }
 }
